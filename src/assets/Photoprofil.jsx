58f365c361d4e70295432436c40d2b701bd0db6f
Nounou <nounourayene20@gmail.com>
2025-04-01 03:34:39 +0100

    Delete src/assets/Photoprofil.jsx

--- src/assets/Photoprofil.jsx
+++
@@ -1,9 +0,0 @@
-function Photoprofil () {
-
-    return (
-          
-            <img src="/home/rayene/Downloads/wp9021845-dinosaur-hd-4k-wallpapers.jpg" alt="Profile" />
-            
-    );   
-}
-export default Photoprofil;
